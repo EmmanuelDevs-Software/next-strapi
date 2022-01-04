098b44596c397bfa0ae659c84c7c47b97071af92
Emmanuel Davila <emmanuel.davila@altran.com>
2022-01-04 06:32:58 +0100

    components and styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,26 @@
-import Head from 'next/head';
-import styles from '../styles/Layout.module.css'
+import Head from "next/head";
+import Header from "./Header";
+import Footer from "./Footer";
+import styles from "../styles/Layout.module.css";
 
-export default function Layout({title, keywords, description, children}) {
-    return (
-        <div>
-            <Head>
-                <title>{title}</title>
-                <meta name='description' content={description} />
-                <meta name='keywords' content={keywords} />
-            </Head>
-           <div className={styles.container}>
-           {children}
-               
-               </div> 
-        </div>
-    )
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <Header />
+      <div className={styles.container}>{children}</div>
+      <Footer/>
+    </div>
+   
+  );
 }
-
-
 
 Layout.defaultProps = {
-    title: 'Dj Events | Find the hottest parties',
-    description: 'Find the latest DJ and other musical events',
-    keywords: 'music,dj,edm,events',
-}
+  title: "Dj Events | Find the hottest parties",
+  description: "Find the latest DJ and other musical events",
+  keywords: "music,dj,edm,events",
+};
